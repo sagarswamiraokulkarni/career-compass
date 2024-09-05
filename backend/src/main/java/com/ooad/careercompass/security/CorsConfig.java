23c952e25430fabdbc81c7a323df9dcd5bc9977d
sagarswamiraokulkarni <147226394+sagarswamiraokulkarni@users.noreply.github.com>
2024-09-04 18:17:48 -0600

    Update CorsConfig.java

--- backend/src/main/java/com/ooad/careercompass/security/CorsConfig.java
+++ backend/src/main/java/com/ooad/careercompass/security/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource(@Value("${app.cors.allowed-origins}") List<String> allowedOrigins) {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(allowedOrigins);
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
